006b0a74b6ebffca436cfd19f04f2b2ca7f1331a
Jan "siOnzee" Tržický <jan@sionzee.cz>
2018-07-21 01:40:49 +0200

    Fix

--- src/observable.js
+++ src/observable.js
@@ -2,7 +2,7 @@ import { proxyToRaw, rawToProxy } from './internals'
 import { storeObservable } from './store'
 import * as builtIns from './builtIns'
 import baseHandlers from './handlers'
-import ProxyPolyfill from 'proxy-polyfill'
+require('proxy-polyfill/proxy.min')
 
 export function observable (obj = {}) {
   // if it is already an observable or it should not be wrapped, return it
@@ -17,7 +17,7 @@ export function observable (obj = {}) {
 function createObservable (obj) {
   // if it is a complex built-in object or a normal object, wrap it
   const handlers = builtIns.getHandlers(obj) || baseHandlers
-  const observable = new ProxyPolyfill(obj, handlers)
+  const observable = new Proxy(obj, handlers)
   // save these to switch between the raw object and the wrapped object with ease later
   rawToProxy.set(obj, observable)
   proxyToRaw.set(observable, obj)
